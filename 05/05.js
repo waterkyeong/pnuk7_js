e50eea226a1dc6f5a3d93cd2f40ad4e2158d6797
waterkyeong <su5368@naver.com>
2024-05-02 14:14:05 +0900

    문자열 처리

--- 05/05.js
+++ 05/05.js
@@ -18,21 +18,30 @@
 /*document.addEventListener('DOMContentLoaded', (e)=>{
  e.preventDefault(); ...*/
 
- const btCreate = (bt, idTxt, captionNode, parentNode)=>{
-    bt.setAttribute("id", idTxt); // 버튼 속성만듬
-    bt.appendChild(captionNode); //자식노드 붙임
-    btDiv4.append(bt);
-}
+
+ const showImg = (imgName)=>{
+    const updownimg = document.querySelector('#updownimg');
+
+    updownimg.setAttribute('src', `./img/${imgName}.png`);
+    updownimg.setAttribute('art', `${imgName}`);
+ }
+
+//  const btCreate = (bt, idTxt, captionNode, parentNode)=>{
+//     bt.setAttribute("id", idTxt); // 버튼 속성만듬
+//     bt.appendChild(captionNode); //자식노드 붙임
+//     btDiv4.append(bt);
+// }
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    const updownimg = document.querySelector('#updownimg');
+    // const updownimg = document.querySelector('#updownimg');
     const input = document.querySelector('#txt1');
     const bt = document.querySelector('#bt');
     const bt2 = document.createElement('button');
     const bt2Txt = document.createTextNode("숫자를 생성해주세요.");
-
     
     
+    
+    let imgName;
     //랜덤수
     let n;
     let flag = false;
@@ -42,6 +51,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
             flag = true  // flag가 true로 바뀌고 밑의 랜덤수 생성, 그리고 flag는 true값을 계속 유지 so, 버튼 계속 눌러도 번호 생성안됨.
             n = Math.floor(Math.random()*100)+1; //1~100
             console.log('n=',n);
+
+            showImg('what'); 
+            bt.textContent = '확인';
+            input.style.display = 'inline'; // 밑에 교수님 코드 차용하면 여기로 다시 돌아와야 하기때문에 설정넣음.
         }
         //입력값 체크
         if(input.value == ''){
@@ -49,32 +62,72 @@ document.addEventListener('DOMContentLoaded', ()=>{
             input.focus();
             return;
         }
-        if(input.value > 100){
-            alert('입력오류');
+        //입력값 체크 : 1~ 100
+        const user = parseInt(input.value);
+        if(user < 1|| user > 100){
+            alert('[입력오류] 1~100사이의 숫자를 입력하세요');
             input.focus();
-            input.value = '';
             return;
         }
-        
-        if(input.value > n){
-            updownimg.setAttribute('src', `./img/down.png`);
-        }
-        else if(input.value < n){
-            updownimg.setAttribute('src', `./img/up.png`);
-        }
-        else{
-            updownimg.setAttribute('src', `./img/good.png`);
-            bt.style.display ='none';
+
+        // if(input.value > 100){
+        //     alert('입력오류 - 1~100사이의 숫자를 입력하세요');
+        //     input.focus();
+        //     return;
+        // }
+        // 위에 두개는 같은거임. 위에거는 by.교수님 밑에거는 by.me
+
+        if(user < n){imgName = 'up';}
+        else if (user > n){imgName ='down';}
+        else {
+            imgName='good';
             input.style.display ='none';
-            btCreate(bt2, "숫자를 생성해주세요", bt2Txt, btDiv4);
-            bt2.addEventListener('click', ()=>{
-                updownimg.setAttribute('src', `./img/start.png`);
-                bt2.style.display = 'none';
-                bt.style.display = 'block';
-                input.style.display = 'block';
-                input.value = "";
-            });   
+            bt.textContent = '숫자를 다시 생성해 주세요.'
+            flag = false;       
         }
+
+        if(imgName === 'up' || imgName === 'down'){
+            input.focus();
+        }
+        
+        showImg(imgName);
+        input.value = '';
+
+
+
+
+        // 여기서부터 밑에는 내가 짠 코드. 
+        //- input 박스 값이 랜덤수 보다 작으면 up이미지
+        // - input 박스 값이 랜덤수 보다 크면 down이미지
+        // - input 박스 값이 랜덤수와 같으면 good이미지
+        //     > input이 보이지 않아야 함
+        //       버튼의 캡션을 '숫자를 생성해주세요'로 변경
+        // ['숫자를 생성해주세요' 버튼]
+        //  - 초기화 : 랜덤수 새로 생성, input 박스 보이기...
+
+        // if(input.value > n){
+        //     updownimg.setAttribute('src', `./img/down.png`);
+        //     updownimg.setAttribute('alt', `down`);
+        // }
+        // else if(input.value < n){
+        //     updownimg.setAttribute('src', `./img/up.png`);
+        //     updownimg.setAttribute('alt', `up`);
+        // }
+        // else{
+        //     updownimg.setAttribute('src', `./img/good.png`);
+        //     updownimg.setAttribute('alt', `good`);
+        //     bt.style.display ='none';
+        //     input.style.display ='none';
+        //     btCreate(bt2, "숫자를 생성해주세요", bt2Txt, btDiv4);
+        //     bt2.addEventListener('click', ()=>{
+        //         updownimg.setAttribute('src', `./img/start.png`);
+        //         updownimg.setAttribute('alt', `start`);
+        //         bt2.style.display = 'none';
+        //         bt.style.display = 'block';
+        //         input.style.display = 'block';
+        //         input.value = "";
+        //     });   
+        // }
         
 
         
